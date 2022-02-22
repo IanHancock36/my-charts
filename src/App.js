1d58e482b6b4c2a6f8e65e4fce608032608ddf96
Ian Hancock <northroadian@gmail.com>
2022-02-22 06:44:24 -0600

    chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
-function App() {
+const data = [
+  {
+    name: "2012",
+    uv: 983000,
+    pv: 1000000,
+    amt: 240000
+  },
+  {
+    name: "2013",
+    uv: 3000,
+    pv: 900000,
+    amt: 2210
+  },
+  {
+    name: "2014",
+    uv: 867000,
+    pv: 900000,
+    amt: 2290
+  },
+  {
+    name: "2015",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "2016",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "2017",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "2018",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "2019",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "2020",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "2022",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  
+ 
+];
+
+export default function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="pv" fill="green" />
+      <Bar dataKey="uv" fill="red" />
+    </BarChart>
   );
 }
-
-export default App;
